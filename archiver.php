8aaf79f85c918a13400f779ca610059b23e2b011
Christoph Burschka <christoph@burschka.de>
2010-02-07 16:00:46 +0100

    Version 4 (overhaul, comments, reparsing partial data)

--- archiver.php
+++ archiver.php
@@ -1,27 +1,59 @@
 <?php
 
+/******************************************************
+ *               Jolt Forum Archiver                  *
+ *           an Ermarian Network production           *
+ *        Arancaytar <arancaytar@ermarian.net>        *
+ *                                                    *
+ * This application can be modified and distributed   *
+ * under the GNU General Public License 3.0 or later. *
+ * Libraries based on the Drupal project (GPL 2) are  *
+ * included.                                          *
+ ******************************************************/
+
+
 require_once 'net/http.inc';
 
+// Set to TRUE to force download of files already stored.
 define('CLOBBER', FALSE);
+
+// Set to TRUE to force re-parsing of local files. Needed only in rare cases of malfunction.
+define('REPARSE', FALSE);
+
+define('URL_JOLT_ROOT', 'http://forums.jolt.co.uk/archive/index.php/');
 define('URL_JOLT_FORUM', 'http://forums.jolt.co.uk/archive/index.php/f-%d.html');
 define('URL_JOLT_FORUM_PAGER', 'http://forums.jolt.co.uk/archive/index.php/f-%d-p-%d.html');
 define('URL_JOLT_TOPIC', 'http://forums.jolt.co.uk/archive/index.php/t-%d.html');
 define('URL_JOLT_TOPIC_PAGER', 'http://forums.jolt.co.uk/archive/index.php/t-%d-p-%d.html');
-define('URL_JOLT_ROOT', 'http://forums.jolt.co.uk/archive/index.php/');
 
 function index_jolt_forum($id, $path = 'files') {
   $start = microtime(TRUE);
   print "Creating path... ";
   r_mkdir("$path/$id/");
   print "$path/$id\n";
+
+  $topics = array();
+  
   print "Determining forum size... ";
   
-  $data = download(sprintf(URL_JOLT_FORUM, $id), "$path/$id/f-$id.html");
+  $data = download(sprintf(URL_JOLT_FORUM, $id), "$path/$id/f-$id.html", TRUE); // force parse.
   preg_match_all("/f-$id(-p-([0-9]+))?\.html/", $data, $pages);
-  $pages = $pages[2][count($pages[2]) - 1];
+  $pages = $pages[2] ? $pages[2][count($pages[2]) - 1] : 1;
   print "$pages pages, ~" . ($pages*250) . " topics\n";
+ 
+  if (file_exists("$path/$id/topics.txt")) {
+    print "Detected existing archive. Reload topics from plain-text index... ";
+    $topic_index = explode("\n", trim(file_get_contents("$path/$id/topics.txt")));
+    foreach ($topic_index as $line) {
+      $line = explode("\t", $line);
+      $topics[$line[0]] = $line[1];
+    }
+    print "reloaded " . count($topics) . " topics.\n";
+  }
+  $before = count($topics);
+  
   preg_match_all('%<a href="t-([0-9]+)\.html">(.+?)</a>%', $data, $topics_raw);
-  $topics = array_combine($topics_raw[1], $topics_raw[2]);
+  if ($topics_raw[1]) $topics += array_combine($topics_raw[1], $topics_raw[2]);
   
   print "Saving forum index... ";
   print_c("1 / $pages, " . sprintf('%0.2f%%', 100 / $pages) . ", " . remaining($start, 1, $pages));
@@ -29,17 +61,22 @@ function index_jolt_forum($id, $path = 'files') {
     $data = download(sprintf(URL_JOLT_FORUM_PAGER, $id, $i), "$path/$id/f-$id-p-$i.html");
     
     preg_match_all('%<a href="t-([0-9]+)\.html">(.+?)</a>%', $data, $topics_raw);
-    $topics += array_combine($topics_raw[1], $topics_raw[2]);
+    if ($topics_raw[1]) $topics += array_combine($topics_raw[1], $topics_raw[2]);
     print_c("$i / $pages, " . sprintf('%0.2f%%', $i * 100 / $pages) . ", " . remaining($start, $i, $pages));
   }
 
   print " done\n";
   print_c();
-  print "Storing topic list in plain text... ";
-  $topic_list = "";
-  foreach ($topics as $tid => $title) $topic_list .= html_entity_decode("$tid\t$title\n");
-  file_put_contents("$path/$id/topics.txt", $topic_list);
-  print "done\n";
+  
+  if ($before != count($topics)) {
+    print "Storing topic list in plain text... ";
+    $topic_list = "";
+    foreach ($topics as $tid => $title) $topic_list .= html_entity_decode("$tid\t$title\n");
+    file_put_contents("$path/$id/topics.txt", $topic_list);
+    print "done\n";
+  }
+  else print "Skipping topic list, as there were no changes.\n";
+  
   $start = microtime(TRUE);
   print "Archiving topics... ";
 
@@ -94,14 +131,14 @@ function format_time($remaining) {
     str_pad(floor($remaining % 60), 2, 0, STR_PAD_LEFT);
 }
 
-function download($url, $local) {
+function download($url, $local, $reparse = REPARSE) {
   if (CLOBBER || !file_exists($local)) {
     $resp = http($url);
     $data = str_replace(URL_JOLT_ROOT, '', $resp->data);
     file_put_contents($local, $data);
     return $data;
   }
-  else {
+  else if ($reparse) {
     return file_get_contents($local);
   }
 }
